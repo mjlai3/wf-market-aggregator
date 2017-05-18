5bf9c88e842c465bfd7e3fca661d96e8d75aec3f
Michael Lai <michaeljin.lai@gmail.com>
2017-05-18 17:43:13 +1000

    Add another ajax call for finding item orders - need to find another solution for calling and storing data, currently calling over 2000 consecutive ajax calls

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -15,13 +15,26 @@ $(() => {
 			url: 'http://warframe.market/api/get_all_items_v2',
 			dataType: 'json',
 			method: 'GET'
-		}).done(function(data) {
+		}).done((data) => {
 			setJSON(data);
+			_.forEach(data, (item) => {
+				callItem(item.item_type, item.item_name);
+			})
 		})
 	}
 
 	function setJSON(data) {
 		itemJSON = data;
 		console.log(itemJSON);
+	}
+
+	function callItem(itemType, itemName) {
+		$.ajax({
+			url: `http://warframe.market/api/get_orders/${itemType}/${itemName}`,
+			dataType: 'json',
+			method: 'GET'
+		}).done((data) => {
+			console.log(data);
+		})
 	}
 });
